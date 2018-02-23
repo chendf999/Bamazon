9310c3738a32e519a982b6066a2f4910413fe6b7
Danfeng Chen <cdf@Danfengs-MacBook-Pro.local>
2018-02-23 15:27:47 -0800

    Done customer function

---
+++ bamazon.sql
@@ -0,0 +1,70 @@
+-- DROP DATABASE bamazon
+-- CREATE DATABASE bamazon
+
+USE bamazon;
+-- 
+-- CREATE TABLE products (
+-- 	item_id INT PRIMARY KEY  AUTO_INCREMENT,
+-- 	product_name VARCHAR(200) NOT NULL,
+-- 	department_name VARCHAR(200) NOT NULL,
+-- 	price DECIMAL(10,2) NOT NULL,
+-- 	stock_quantity INT NOT NULL
+-- );
+
+-- INSERT INTO products (product_name, department_name, price, stock_quantity)
+-- VALUES ("A Playcentric Approach to Creating Innovative Games", "Art & Design", 25.95, 4);
+-- 
+-- INSERT INTO products (product_name, department_name, price, stock_quantity)
+-- VALUES ("JavaScript: The Definitive Guide", "Computer Science", 21.92, 10);
+-- 
+-- INSERT INTO products (product_name, department_name, price, stock_quantity)
+-- VALUES ("Living with Art ", "Art & Design", 159.33, 5);
+-- 
+-- INSERT INTO products (product_name, department_name, price, stock_quantity)
+-- VALUES ("Web Design with HTML, CSS, JavaScript and jQuery Set", "Computer Science", 28, 8);
+--
+-- INSERT INTO products (product_name, department_name, price, stock_quantity)
+-- VALUES ("Sprint: How To Solve Big Problems and Test New Ideas in Just Five Days", "Art & Design", 14.99, 4);
+-- 
+-- INSERT INTO products (product_name, department_name, price, stock_quantity)
+-- VALUES ("CSS: The Missing Manual", "Computer Science", 29.99, 6);
+-- 
+-- INSERT INTO products (product_name, department_name, price, stock_quantity)
+-- VALUES ("Python Tricks: A Buffet of Awesome Python Features ", "Computer Science", 26.99, 5);
+-- 
+-- INSERT INTO products (product_name, department_name, price, stock_quantity)
+-- VALUES ("Mapping Experiences: A Complete Guide to Creating Value through Journeys, Blueprints, and Diagrams", "Art & Design", 20.98, 2);
+-- 
+-- INSERT INTO products (product_name, department_name, price, stock_quantity)
+-- VALUES ("Python Tricks: A Buffet of Awesome Python Features ", "History", 26.99, 5);
+-- 
+-- INSERT INTO products (product_name, department_name, price, stock_quantity)
+-- VALUES ("Command and Control: Nuclear Weapons, the Damascus Accident, and the Illusion of Safety", "History", 13.7, 1);
+
+-- INSERT INTO products (product_name, department_name, price, stock_quantity)
+-- VALUES ("The Story of Britain: From the Romans to the Present", "History", 17.96, 3);
+-- 
+-- INSERT INTO products (product_name, department_name, price, stock_quantity)
+-- VALUES ("Arduino Electronics Blueprints", "Computer Science", 46.99, 2);
+-- 
+-- INSERT INTO products (product_name, department_name, price, stock_quantity)
+-- VALUES ("Introduction to Algorithms", "Computer Science", 51, 4);
+-- 
+-- INSERT INTO products (product_name, department_name, price, stock_quantity)
+-- VALUES ("C Programming Language", "Computer Science", 35.64, 12);
+
+-- INSERT INTO products (product_name, department_name, price, stock_quantity)
+-- VALUES ("The Elements of Typographic Style", "Art & Design", 37.95, 9);
+-- 
+-- INSERT INTO products (product_name, department_name, price, stock_quantity)
+-- VALUES ("Emotionally Durable Design: Objects, Experiences and Empathy", "Art & Design", 51.95, 4);
+-- 
+
+-- SELECT COUNT(item_id)
+-- FROM products;
+
+-- UPDATE products
+-- SET stock_quantity = 50
+-- WHERE item_id BETWEEN 1 AND 16; 
+
+SELECT * FROM products
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -4,15 +4,18 @@ require('dotenv').config();
 const mysql = require('mysql');
 const inquirer = require('inquirer');
 
-const mysqlPass = process.env.PASSWORD;
+const password = process.env.PASSWORD;
 
 const connection = mysql.createConnection({
 	host: "localhost",
-    port: 3306,
-    user: "root",
-    password: mysqlPass,
-    database: "bamazon"
+	port: 3306,
+	user: "root",
+	password: password,
+	database: "bamazon"
 });
+
+var itemNum = 0;
+var items = [];
 
 connection.connect(function(err){
 	if (err) {
@@ -22,9 +25,86 @@ connection.connect(function(err){
 			if (err) {
 				console.log(err);
 			} else {
-				console.log(data);
+				itemNum = data.length;
+				for (var i = 0; i < data.length; i++) {
+					items.push(data[i]);
+				}
+				list_item();
 			}
 		});
 	}
 });
 
+function list_item(){
+	for (var i = 0; i < items.length; i++) {
+		console.log(items[i].item_id + '. ' + items[i].product_name + '\n Department: ' + items[i].department_name + '\n Price: ' + items[i].price + '   Stock: ' + items[i].stock_quantity);
+	}
+	add_item();
+}
+
+function add_item(){
+	inquirer.prompt([
+		// question 1: select item -------------------------------/
+		{
+			type: "input",
+			message: "Enter the item id of the book you would like to buy",
+			name: "item_id",
+			validate: function(value){
+				if(isNaN(value) === false && parseInt(value) > 0 && parseInt(value) <= itemNum){
+					return true
+				} else {
+					console.log('\n Input item id between 1-' + itemNum + '.');
+					return false;
+				}
+			}
+		},
+		// question 2: quantity -------------------------------/
+		{
+			type: "input",
+			message: "How many do you need?",
+			name: "quantity",
+			validate: function(value){
+				if (!isNaN(value) && parseInt(value) > 0) {
+					return true;
+				} else {
+					console.log('Please enter a valid number.');
+				}
+			}
+		}
+	]).then(function(value){
+		connection.query('SELECT item_id, product_name, department_name, price, stock_quantity FROM products WHERE item_id = ' + value.item_id, function(err, data) {
+			if (err) {
+				console.log(err);
+			} else {
+				if (value.quantity > data[0].stock_quantity) {
+					console.log('\n Insufficient quantity. Only ' + data[0].stock_quantity + ' available.');
+					add_item();
+				} else {
+					var total = parseInt(data[0].price) * parseInt(value.quantity);
+					var quantity_update = data[0].stock_quantity - value.quantity;
+
+					connection.query('UPDATE products SET stock_quantity = ' + quantity_update + ' WHERE item_id = ' + value.item_id);
+					console.log('Your total is $' + total + '.');
+					continue_shopping();
+				}
+			}
+		});
+	});
+}
+
+function continue_shopping(){
+	inquirer.prompt([
+		{
+			type: "list",
+			message: "Continue Shopping?",
+			choices: ['Yes, please.', 'No, thanks'],
+			name: "next"
+		}
+	]).then(function(value){
+		if (value.next === 'Yes, please.') {
+			list_item();
+		} else {
+			console.log('Thank you for shopping with us!');
+		}
+	});
+}
